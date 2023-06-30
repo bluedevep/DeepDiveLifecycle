7921b518457d8281c3786055c6a12072e43d536e
bluedevep <sara.sorhouet@gmail.com>
2023-06-30 22:05:25 +0200

    First commit

---
+++ src/components/workflow.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Workflow extends Component {
+  render() {
+    return (
+      <h1>
+        Workflow...
+      </h1>
+    );
+  }
+}
